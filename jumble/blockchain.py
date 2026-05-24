60170e7f071231bced96230350e6e92a1758a35d
Betz Lenteria <wilbertlenteria3@gmail.com>
2026-05-24 12:25:40 +0800

    Create blockchain.py

---
+++ jumble/blockchain.py
@@ -0,0 +1,8 @@
+import logging
+from minigames.blockchain import minigames_blockchain
+
+logger = logging.getLogger(__name__)
+
+jumble_blockchain = minigames_blockchain
+
+logger.info("🔤 Jumble Words using shared Minigames Blockchain Service")
